609e10148944f29c7d7410f0b48255ba79485889
Urs Roesch <github@bun.ch>
2023-05-29 14:29:16 +0200

    Release v1.2023.8-alpha2-uroesch
    Summary:
      * Upstream release v1.2023.8

--- App/plantuml-server.cmd
+++ App/plantuml-server.cmd
@@ -1,16 +1,32 @@
 @echo off
+rem -----------------------------------------------------------------
+rem  This is a wrapper script to start the webfrontend for PlantUML
+rem  and start the default browser.
+rem -----------------------------------------------------------------
 
+rem -----------------------------------------------------------------
+rem  Globals
+rem -----------------------------------------------------------------
+set PLANTUML_URL=http://localhost:8080/plantuml/
 set TOMCAT_DIR=%~dp0apache-tomcat
 set BROWSER_OPTIONS=
 
+rem -----------------------------------------------------------------
+rem  Start Tomcat and PlantUML
+rem -----------------------------------------------------------------
 cd /d %TOMCAT_DIR%\bin 
 start /b catalina.bat run
 
-
+rem -----------------------------------------------------------------
+rem  Start Browser
+rem -----------------------------------------------------------------
 if x%PORTABLE_APPS_BROWSER:firefox=%==x%PORTABLE_APPS_BROWSER% (
   echo Firefox
   set BROWSER_OPTIONS=-new-tab  
 )
 
-rem start /b %PORTABLE_APPS_BROWSER% %BROWSER_OPTIONS% http://localhost:8080/plantuml/	
-start /b http://localhost:8080/plantuml/	
+if x%PORTABLE_APPS_BROWSER%==x (
+  start /b %PLANTUML_URL%
+) else (
+  start /b %PORTABLE_APPS_BROWSER% %BROWSER_OPTIONS% %PLANTUML_URL% 
+)
